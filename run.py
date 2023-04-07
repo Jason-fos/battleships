8c734d0586e5b18239b5af961e5185ad9623f534
Jason-fos <jason.fos2812@outlook.com>
2023-04-07 10:48:57 +0000

    initial commit, write code for cpu and player boards

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Battleships terminal game written in python
+# Import modules
+import random
+import sys
+import time
+
+# CPU and player boards, small and big
+player_board_small = [[''] * 6 for x in range(6)]
+cpu_board_small = [[''] * 6 for x in range(6)]
+player_board_big = [[''] * 9 for x in range(9)]
+cpu_board_big = [[''] * 9 for x in range(9)]
