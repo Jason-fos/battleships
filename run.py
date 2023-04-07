9841bef643addbf25c307a949ce70ed72d73bf61
Jason-fos <jason.fos2812@outlook.com>
2023-04-07 11:32:19 +0000

    add two typing effect functions for print statements, a fast and slow verwsion

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-# Battleships terminal game written in python
+"""
+Battleships terminal game written in python
+
+"""
 # Import modules
 import random
 import sys
@@ -9,3 +12,32 @@ player_board_small = [[''] * 6 for x in range(6)]
 cpu_board_small = [[''] * 6 for x in range(6)]
 player_board_big = [[''] * 9 for x in range(9)]
 cpu_board_big = [[''] * 9 for x in range(9)]
+
+# Dictionary to form key value pairs for board co-ordinates
+coordinates = {
+    'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8
+}
+# Number of ships and their lengths for boards
+ships_small = [2, 3, 4, 5]
+ships_big = [2, 3, 3, 4, 4, 5]
+
+
+def print_fast(string):
+    """
+    Creates fast typing effect for introduction/logo
+    """
+    for letter in string:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def print_slow(string):
+    """
+    Creates a slow typing effect for short print statements
+
+    """
+    for letter in string:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.1)
